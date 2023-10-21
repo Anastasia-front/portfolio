aaa9e439aef9331c81720e3eec0bddd889d70dc3
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-10-21 16:38:52 +0300

    refactor : add code in Portal component

---
+++ app/[locale]/components/Portal/Portal.tsx
@@ -0,0 +1,43 @@
+import type { ReactElement } from "react";
+import { useLayoutEffect, useState } from "react";
+import { createPortal } from "react-dom";
+
+const createWrapperElement = (wrapperId: string) => {
+  if (!document) return null;
+  const wrapperElement = document.createElement("div");
+  wrapperElement.setAttribute("id", wrapperId);
+  document.body.appendChild(wrapperElement);
+  return wrapperElement;
+};
+
+interface PortalProps {
+  children: ReactElement;
+  wrapperId: string;
+}
+
+export function Portal({ children, wrapperId }: PortalProps) {
+  const [wrapperElement, setWrapperElement] = useState<HTMLElement>();
+  useLayoutEffect(() => {
+    let el = document.getElementById(wrapperId);
+    let systemCreated = false;
+
+    if (!el) {
+      systemCreated = true;
+      el = createWrapperElement(wrapperId);
+    }
+
+    if (el) {
+      setWrapperElement(el);
+    }
+
+    return () => {
+      if (systemCreated && el?.parentNode) {
+        el.parentNode.removeChild(el);
+      }
+    };
+  }, [wrapperId]);
+
+  if (!wrapperElement) return null;
+
+  return createPortal(children, wrapperElement);
+}
