64808f4fa21a3dcba47a1c9a4c9b8999ce16460b
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-10-24 15:40:00 +0300

    refactor : add adaptive styles and some tags to contacts page

--- app/[locale]/contacts/page.tsx
+++ app/[locale]/contacts/page.tsx
@@ -2,22 +2,35 @@
 
 import { useTranslations } from "next-intl";
 
+import { motion } from "framer-motion";
+
 import { Form, LottiePlayer } from "@/components";
+import { gridVariants } from "@/utils";
 
 export default function ContactsPage() {
   const t = useTranslations("contacts");
 
   return (
-    <section id="contact" className="contact container">
-      <Form />
-      <LottiePlayer
-        src="https://assets10.lottiefiles.com/packages/lf20_lstnp9p3.json"
-        background="transparent"
-        speed={1}
-        className="contact__animation"
-        loop
-        autoplay
-      />
-    </section>
+    <main className="contact-page container">
+      <div className="page-headings">
+        <motion.h1 variants={gridVariants} initial="hidden" animate="visible">
+          {t("title")}
+        </motion.h1>
+        <motion.h6 variants={gridVariants} initial="hidden" animate="visible">
+          {t("description")}
+        </motion.h6>
+      </div>
+      <div className="contact-block">
+        <Form />
+        <LottiePlayer
+          src="https://assets10.lottiefiles.com/packages/lf20_lstnp9p3.json"
+          background="transparent"
+          speed={1}
+          className="contact__animation"
+          loop
+          autoplay
+        />
+      </div>
+    </main>
   );
 }
