e508b2cd6a9b58949f9fc4e54c7059a2074dd786
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-11-10 14:31:51 +0200

    refactor : comment ProgressBar everywhere

--- app/[locale]/components/index.ts
+++ app/[locale]/components/index.ts
@@ -19,7 +19,7 @@ export * from "./LottiePlayer";
 export * from "./Navigation";
 export * from "./Portal";
 export * from "./Progress";
-export * from "./ProgressBar";
+// export * from "./ProgressBar";
 export * from "./ProjectItem";
 export * from "./Projects";
 export * from "./Settings";
