38dda1f39a63dad349022eaa93770986fa9ba47e
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-11-20 23:34:18 +0200

    refactor : delete useless fragment from BurgerMenu

--- app/[locale]/components/BurgerMenu/BurgerMenu.tsx
+++ app/[locale]/components/BurgerMenu/BurgerMenu.tsx
@@ -45,43 +45,41 @@ export function BurgerMenu() {
         <div className="icon"></div>
       </button>
       <Portal wrapperId="portal">
-        <>
+        <div
+          className={`burger-backdrop ${
+            isMenuOpen ? "burger-backdrop__active" : ""
+          } `}
+          onClick={handleBackdropClick}
+        >
           <div
-            className={`burger-backdrop ${
-              isMenuOpen ? "burger-backdrop__active" : ""
+            className={`burger-background ${
+              isMenuOpen && "burger-background__active"
             } `}
-            onClick={handleBackdropClick}
           >
-            <div
-              className={`burger-background ${
-                isMenuOpen && "burger-background__active"
-              } `}
+            <button
+              type="button"
+              className={`button-icon button-icon__close animated-icon ${classNameActive}`}
+              onClick={hideBurger}
             >
-              <button
-                type="button"
-                className={`button-icon button-icon__close animated-icon ${classNameActive}`}
-                onClick={hideBurger}
-              >
-                <div className="icon"></div>
-              </button>
+              <div className="icon"></div>
+            </button>
 
-              <div className="burger-column">
-                <Navigation onClick={hideBurger} />
-                <Switchers className="burger-switchers" />
-                <Link href="/" className="logo">
-                  <Image
-                    className="logo__img"
-                    priority={true}
-                    src={logo}
-                    alt={t("logo")}
-                    width={isScreenTabletSm ? 30 : 50}
-                    height={isScreenTabletSm ? 30 : 50}
-                  />
-                </Link>
-              </div>
+            <div className="burger-column">
+              <Navigation onClick={hideBurger} />
+              <Switchers className="burger-switchers" />
+              <Link href="/" className="logo">
+                <Image
+                  className="logo__img"
+                  priority={true}
+                  src={logo}
+                  alt={t("logo")}
+                  width={isScreenTabletSm ? 30 : 50}
+                  height={isScreenTabletSm ? 30 : 50}
+                />
+              </Link>
             </div>
           </div>
-        </>
+        </div>
       </Portal>
     </>
   );
