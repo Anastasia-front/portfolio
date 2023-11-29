07ca0ddd03e149cd054459b95f100e769f825fd7
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-11-29 17:48:33 +0200

    fix : add logic of play video

--- app/[locale]/components/Video/Video.tsx
+++ app/[locale]/components/Video/Video.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 import { useTheme } from "next-themes";
 import { usePathname } from "next/navigation";
@@ -27,7 +27,48 @@ export function Video() {
     }
   })();
 
+  useEffect(() => {
+    const playVideo = async () => {
+      if (videoRef.current) {
+        try {
+          await videoRef.current.play();
+        } catch (error) {
+          console.error("Autoplay failed:", error);
+        }
+      }
+    };
+
+    // Delay autoplay after component mount
+    const delay = setTimeout(() => {
+      playVideo();
+    }, 1000); // Adjust the delay time as needed
+
+    return () => clearTimeout(delay);
+  }, []);
+
+  const handleInteraction = () => {
+    if (videoRef.current) {
+      videoRef.current.play().catch((error) => {
+        console.error("Autoplay failed:", error);
+      });
+    }
+  };
+
+  useEffect(() => {
+    // You can attach this event listener to any interactive element
+    document.addEventListener("click", handleInteraction);
+
+    return () => {
+      document.removeEventListener("click", handleInteraction);
+    };
+  }, []);
+
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  return <video className="video" autoPlay ref={videoRef} src={video} />;
+  return (
+    <>
+      <video className="video" autoPlay ref={videoRef} src={video} />
+      <p className="video-tip">some tips</p>
+    </>
+  );
 }
