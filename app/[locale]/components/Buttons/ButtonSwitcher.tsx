b21e060406d6aae9fb1bb8545efed7a13c473ef1
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-12-12 16:58:42 +0200

    fix : change ButtonSwitcher

--- app/[locale]/components/Buttons/ButtonSwitcher.tsx
+++ app/[locale]/components/Buttons/ButtonSwitcher.tsx
@@ -2,7 +2,6 @@ import { ForwardedRef, forwardRef, memo } from "react";
 
 import Image from "next/image";
 
-
 interface Props {
   icon?: JSX.Element;
   imgUrl?: string;
@@ -15,42 +14,28 @@ interface Props {
 export const ButtonSwitcher = memo(
   forwardRef(
     (
-      {
-        imgUrl,
-        className,
-        onClick,
-        icon,
-        title = "",
-        alt,
-      }: Props,
-      ref: ForwardedRef<HTMLDivElement>
+      { imgUrl, className, onClick, icon, title = "", alt }: Props,
+      ref: ForwardedRef<HTMLButtonElement>
     ) => {
       return (
-        <div
-          title={title}
-          className={
-            className ? `button-container ${className}` : "button-container"
-          }
+        <button
           ref={ref}
+          title={title}
+          className={`button-switcher  ${className ? className : ""}`}
+          type="button"
+          onClick={onClick}
         >
-          <button
-            className="button-switcher"
-            type="button"
-            onClick={onClick}
-            style={{ transition: "var(--transition)" }}
-          >
-            {imgUrl && (
-              <Image
-                className="button-img"
-                src={imgUrl}
-                width={20}
-                height={20}
-                alt={alt}
-              />
-            )}
-            {icon && icon}
-          </button>
-        </div>
+          {imgUrl && (
+            <Image
+              className="button-img"
+              src={imgUrl}
+              width={20}
+              height={20}
+              alt={alt}
+            />
+          )}
+          {icon && icon}
+        </button>
       );
     }
   )
