c906b0896e32edb6ade0c6c2f7f932c4f91cb8c7
Anastasia-front <palitsanastasia3.ap@gmail.com>
2024-02-28 09:28:07 +0200

    refactor : update getLocaleStorageItem.ts

--- app/[locale]/helpers/getLocaleStorageItem.ts
+++ app/[locale]/helpers/getLocaleStorageItem.ts
@@ -1,9 +1,17 @@
-export function getLocaleStorageItem<T>(key: string): T | null {
-  const data: string | null = localStorage.getItem(key);
-
-  if (data !== null) {
-    return JSON.parse(data);
+export function getLocaleStorageItem<T>(key: string, defaultValue: T): T {
+  if (typeof window !== 'undefined') {
+    const data: string | null = localStorage.getItem(key);
+  
+    if (data !== null) {
+      try {
+        // Attempt to parse the data as JSON
+        return JSON.parse(data);
+      } catch (error) {
+        // If parsing fails, return the data as a simple string
+        return data as T;
+      }
+    }
   }
-
-  return null;
+  
+  return defaultValue;
 }
