9f98ef216eb2a14756fa9aa03bf4040c7bb899b0
Anastasia-front <palitsanastasia3.ap@gmail.com>
2024-03-15 18:10:36 +0200

    refactor : change useScrollLock

--- src/app/[locale]/hooks/useScrollLock.ts
+++ src/app/[locale]/hooks/useScrollLock.ts
@@ -2,16 +2,15 @@ import { useCallback } from "react";
 
 export function useScrollLock(): Record<string, () => void> {
   const lockScroll = useCallback(() => {
-    const scrollBarCompensation = window.innerWidth - document.body.offsetWidth;
     document.body.style.overflow = "hidden";
     document.body.style.width = "100%";
-    document.body.style.paddingRight = `${scrollBarCompensation}px`;
+    document.body.style.scrollbarGutter = "stable";
   }, []);
 
   const unlockScroll = useCallback(() => {
     document.body.style.overflow = "";
     document.body.style.width = "auto";
-    document.body.style.paddingRight = "0px";
+    document.body.style.scrollbarGutter = "auto";
   }, []);
 
   return { lockScroll, unlockScroll };
