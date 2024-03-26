a1a5260bbb2a369aaeda61864c7990bc93245c78
Prysiazhna <prysiazhnastasia@gmail.com>
2024-03-26 23:31:45 +0200

    refactor : roll back middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,47 +1,18 @@
+import { type Locale, locales } from "./locales";
 import createMiddleware from "next-intl/middleware";
-import { NextResponse, type NextRequest } from "next/server";
-
-import { EN, locales, type Locale } from "./locales";
-
-// Custom middleware function to set custom headers
-export async function middleware(request: NextRequest): Promise<Response> {
-  // Create a new Headers object from the request headers
-  const requestHeaders = new Headers(request.headers);
-  // Set "x-url" header with the request URL pathname
-  requestHeaders.set("x-url", request.url);
-
-  // Call the nextIntlMiddleware to get the response
-  const nextIntlResponse = await nextIntlMiddleware(request);
-
-  // Clone the headers of the nextIntlResponse
-  const responseHeaders = new Headers(nextIntlResponse.headers);
-
-  // Set the "x-url" header in the responseHeaders
-  responseHeaders.set("x-url", request.url);
-
-  // Create a new Response with the existing body, updated headers, and other properties
-  const customResponse: Response = new NextResponse(nextIntlResponse.body, {
-    headers: responseHeaders,
-    status: nextIntlResponse.status,
-    statusText: nextIntlResponse.statusText,
-  });
-
-  return customResponse;
-}
+import { type NextRequest, type NextResponse } from "next/server";
 
 const nextIntlMiddleware = createMiddleware({
   locales,
-  defaultLocale: EN as Locale,
+  defaultLocale: "en" satisfies Locale,
   localePrefix: "never",
 });
 
-// Exporting default middleware function
 // eslint-disable-next-line import/no-anonymous-default-export
-export default async function (req: NextRequest): Promise<Response> {
-  return middleware(req);
+export default function (req: NextRequest): NextResponse {
+  return nextIntlMiddleware(req);
 }
 
-// Configuration for the middleware
 export const config = {
   // match only internationalized pathnames
   matcher: [
