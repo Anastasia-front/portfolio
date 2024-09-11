cb60f41242e82656b2fc1a646b0005414bda4461
Anastasia Prysiazhna <prysiazhnastasia@gmail.com>
2024-09-11 11:04:40 +0200

    change robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/private/",
+      // disallow: "/private/",
     },
     sitemap: `${BASE_URL}/sitemap.xml`,
   };
